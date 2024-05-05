6ba599b8d3f9912ea059169733f5da896f81d717
Anthony Colaiacovo <anthonyecolaiacovo@gmail.com>
2024-05-04 21:00:08 -0400

    Init + Basic Window

---
+++ ac_editor.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+def initialize_gui():
+    window = Tk()
+    window.title("ac_editor")
+
+    return window
+
+
+if (__name__ == "__main__"):
+    window = initialize_gui()
+    window.mainloop()
