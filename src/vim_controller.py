89bf6f1b3b9352b9be73adb8e88f894c7a907f84
Anthony Colaiacovo <anthonyecolaiacovo@gmail.com>
2024-09-11 11:21:54 -0400

    Create testable backbone for handling vim commands

--- src/vim_controller.py
+++ src/vim_controller.py
@@ -1,3 +1,5 @@
+import re
+
 class VimController:
     NORMAL = 0
     INSERT = 1
@@ -7,12 +9,42 @@ class VimController:
     message = normal_message
     buffer = ""
 
+    valid_commands = {
+        "[0-9]+h" : lambda: 10,
+        "[0-9]+j" : lambda: 10,
+        "[0-9]+k" : lambda: 10,
+        "[0-9]+l" : lambda: 10,
+        "i"       : lambda: 10,
+        "A"       : lambda: 10,
+        "\^"      : lambda: 10,
+        "\$"      : lambda: 10, 
+        ":w"      : lambda: 10,
+        ":q"      : lambda: 10, 
+        ":wq"     : lambda: 10, 
+        ":q!"     : lambda: 10, 
+    }
+
+
     def display_message(self, label):
         label.config(text=self.message)
         label.pack()
 
     def clear_buffer(self):
         self.buffer = ""
+    
+    def is_valid_command(self, command):
 
-    def interpret_buffer(self):
-        return
+        for regex in self.valid_commands:
+            if re.match(regex, command):
+                return (True, regex)
+        return (False, None)
+
+    def interpret_buffer(self):  
+        values = self.is_valid_command(self.buffer)
+        valid = values[0]
+        regex = values[1]
+
+        if valid:
+            self.clear_buffer()
+
+
